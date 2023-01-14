4532c71057d48b98187e26260513c30f340cd489
guoriyue <1800012773@pku.edu.cn>
2023-01-13 23:46:02 -0800

    task 1 & task 2, pointer allocation and free error

---
+++ draw-svg-master/build/CS248/deps/glew/CMakeFiles/glew.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/guomingfei/Desktop/cs248/draw-svg-master/CS248/deps/glew/src/glew.c" "CS248/deps/glew/CMakeFiles/glew.dir/src/glew.c.o" "gcc" "CS248/deps/glew/CMakeFiles/glew.dir/src/glew.c.o.d"
+  "/Users/guomingfei/Desktop/cs248/draw-svg-master/CS248/deps/glew/src/glewinfo.c" "CS248/deps/glew/CMakeFiles/glew.dir/src/glewinfo.c.o" "gcc" "CS248/deps/glew/CMakeFiles/glew.dir/src/glewinfo.c.o.d"
+  "/Users/guomingfei/Desktop/cs248/draw-svg-master/CS248/deps/glew/src/visualinfo.c" "CS248/deps/glew/CMakeFiles/glew.dir/src/visualinfo.c.o" "gcc" "CS248/deps/glew/CMakeFiles/glew.dir/src/visualinfo.c.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ draw-svg-master/build/CS248/src/CMakeFiles/CS248.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/guomingfei/Desktop/cs248/draw-svg-master/CS248/src/base64.cpp" "CS248/src/CMakeFiles/CS248.dir/base64.cpp.o" "gcc" "CS248/src/CMakeFiles/CS248.dir/base64.cpp.o.d"
+  "/Users/guomingfei/Desktop/cs248/draw-svg-master/CS248/src/color.cpp" "CS248/src/CMakeFiles/CS248.dir/color.cpp.o" "gcc" "CS248/src/CMakeFiles/CS248.dir/color.cpp.o.d"
+  "/Users/guomingfei/Desktop/cs248/draw-svg-master/CS248/src/complex.cpp" "CS248/src/CMakeFiles/CS248.dir/complex.cpp.o" "gcc" "CS248/src/CMakeFiles/CS248.dir/complex.cpp.o.d"
+  "/Users/guomingfei/Desktop/cs248/draw-svg-master/CS248/src/lodepng.cpp" "CS248/src/CMakeFiles/CS248.dir/lodepng.cpp.o" "gcc" "CS248/src/CMakeFiles/CS248.dir/lodepng.cpp.o.d"
+  "/Users/guomingfei/Desktop/cs248/draw-svg-master/CS248/src/matrix3x3.cpp" "CS248/src/CMakeFiles/CS248.dir/matrix3x3.cpp.o" "gcc" "CS248/src/CMakeFiles/CS248.dir/matrix3x3.cpp.o.d"
+  "/Users/guomingfei/Desktop/cs248/draw-svg-master/CS248/src/matrix4x4.cpp" "CS248/src/CMakeFiles/CS248.dir/matrix4x4.cpp.o" "gcc" "CS248/src/CMakeFiles/CS248.dir/matrix4x4.cpp.o.d"
+  "/Users/guomingfei/Desktop/cs248/draw-svg-master/CS248/src/osdfont.cpp" "CS248/src/CMakeFiles/CS248.dir/osdfont.cpp.o" "gcc" "CS248/src/CMakeFiles/CS248.dir/osdfont.cpp.o.d"
+  "/Users/guomingfei/Desktop/cs248/draw-svg-master/CS248/src/osdtext.cpp" "CS248/src/CMakeFiles/CS248.dir/osdtext.cpp.o" "gcc" "CS248/src/CMakeFiles/CS248.dir/osdtext.cpp.o.d"
+  "/Users/guomingfei/Desktop/cs248/draw-svg-master/CS248/src/quaternion.cpp" "CS248/src/CMakeFiles/CS248.dir/quaternion.cpp.o" "gcc" "CS248/src/CMakeFiles/CS248.dir/quaternion.cpp.o.d"
+  "/Users/guomingfei/Desktop/cs248/draw-svg-master/CS248/src/spectrum.cpp" "CS248/src/CMakeFiles/CS248.dir/spectrum.cpp.o" "gcc" "CS248/src/CMakeFiles/CS248.dir/spectrum.cpp.o.d"
+  "/Users/guomingfei/Desktop/cs248/draw-svg-master/CS248/src/tinyxml2.cpp" "CS248/src/CMakeFiles/CS248.dir/tinyxml2.cpp.o" "gcc" "CS248/src/CMakeFiles/CS248.dir/tinyxml2.cpp.o.d"
+  "/Users/guomingfei/Desktop/cs248/draw-svg-master/CS248/src/vector2D.cpp" "CS248/src/CMakeFiles/CS248.dir/vector2D.cpp.o" "gcc" "CS248/src/CMakeFiles/CS248.dir/vector2D.cpp.o.d"
+  "/Users/guomingfei/Desktop/cs248/draw-svg-master/CS248/src/vector3D.cpp" "CS248/src/CMakeFiles/CS248.dir/vector3D.cpp.o" "gcc" "CS248/src/CMakeFiles/CS248.dir/vector3D.cpp.o.d"
+  "/Users/guomingfei/Desktop/cs248/draw-svg-master/CS248/src/vector4D.cpp" "CS248/src/CMakeFiles/CS248.dir/vector4D.cpp.o" "gcc" "CS248/src/CMakeFiles/CS248.dir/vector4D.cpp.o.d"
+  "/Users/guomingfei/Desktop/cs248/draw-svg-master/CS248/src/viewer.cpp" "CS248/src/CMakeFiles/CS248.dir/viewer.cpp.o" "gcc" "CS248/src/CMakeFiles/CS248.dir/viewer.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/Users/guomingfei/Desktop/cs248/draw-svg-master/build/CS248/deps/glew/CMakeFiles/glew.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
