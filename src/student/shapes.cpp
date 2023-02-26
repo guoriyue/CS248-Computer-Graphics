7e696a292699ebb0f4af09aef245d80f4905f12e
guoriyue <1800012773@pku.edu.cn>
2023-02-25 18:39:37 -0800

    finish extra credit 1

--- src/student/shapes.cpp
+++ src/student/shapes.cpp
@@ -31,6 +31,39 @@ Trace Sphere::hit(const Ray& ray) const {
     ret.distance = 0.0f;   // at what distance did the intersection occur?
     ret.position = Vec3{}; // where was the intersection?
     ret.normal = Vec3{};   // what was the surface normal at the intersection?
+
+    float t0;
+    float t1;
+
+    Vec3 L = bbox().center() - ray.point;
+    float a = dot(ray.dir, ray.dir);
+    float b = 2 * dot(L, ray.dir);
+    float c = dot(L, L) - 1;
+
+    float discriminant = b * b - 4 * a * c;
+    if (discriminant < 0) {
+        return ret;
+    }
+    else {
+        t0 = (-b - sqrt(discriminant)) / (2 * a);
+        t1 = (-b + sqrt(discriminant)) / (2 * a);
+    }
+
+    if (ray.dist_bounds[0] <= t0 && t0 <= ray.dist_bounds[1]) {
+        ret.hit = true;
+        ret.distance = t0;
+        ret.position = ray.at(t0);
+        ret.normal = (ret.position - bbox().center()).unit();
+        return ret;
+    }
+    if (ray.dist_bounds[0] <= t1 && t1 <= ray.dist_bounds[1]) {
+        ret.hit = true;
+        ret.distance = t1;
+        ret.position = ray.at(t1);
+        ret.normal = (ret.position - bbox().center()).unit();
+        return ret;
+    }
+
     return ret;
 }
 
